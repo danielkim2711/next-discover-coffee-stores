41fe0b9da954f51a11dee2053a9bbb6de80cf2d7
Daniel Kim <danielkim2711@gmail.com>
2022-09-14 17:22:06 +1200

    Add more styles to banner and home component

--- components/banner.js
+++ components/banner.js
@@ -7,10 +7,12 @@ const Banner = ({ handleOnClick, buttonText }) => {
         <span className={styles.title1}>Coffee</span>
         <span className={styles.title2}>Connoisseur</span>
       </h1>
-      <p className={styles.subtitle}>Discover your local coffee shops!</p>
-      <button className={styles.button} onClick={handleOnClick}>
-        {buttonText}
-      </button>
+      <p className={styles.subTitle}>Discover your local coffee shops!</p>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={handleOnClick}>
+          {buttonText}
+        </button>
+      </div>
     </div>
   );
 };
